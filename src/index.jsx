f955c3c2e17cb6020cf8f1539ad455405525d5e5
Derek Johnson <derek@dereks-mbp.gateway.sonic.net>
2018-05-17 12:59:18 -0700

    Fix react-select conflict

--- src/index.jsx
+++ src/index.jsx
@@ -266,7 +266,7 @@ export default class DatePicker extends React.Component {
 
   deferFocusInput = () => {
     this.cancelFocusInput();
-    this.inputFocusTimeout = setTimeout(() => this.setFocus(), 1);
+    //this.inputFocusTimeout = setTimeout(() => this.setFocus(), 1);
   };
 
   handleDropdownFocus = () => {
